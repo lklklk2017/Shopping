4396ff8e85899bd27832137c4a9b1480eb4b2142
caixiaojie <m18408249795@163.com>
2017-01-04 16:40:22 +0800

    蔡小杰1.4 16：42测试提交shopcar

---
+++ app/src/main/java/com/qf/nwt/application/callback/OnNumDescListener.java
@@ -0,0 +1,11 @@
+package com.qf.nwt.application.callback;
+
+import com.qf.nwt.application.bean.ShopCarDetailEntity;
+
+/**
+ * Created by Administrator on 2017/1/3 0003.
+ */
+
+public interface OnNumDescListener {
+    void onNumDescListener();
+}
